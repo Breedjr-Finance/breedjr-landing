1d1e191bd33c47e02d882020de6051f67d77f825
Michael Akinsuroju <makinsuroju@gmail.com>
2025-06-08 12:20:20 +0100

    fix: fonts not found

--- app/fonts.ts
+++ app/fonts.ts
@@ -5,12 +5,12 @@ export const agrandir = localFont({
   display: "swap",
   src: [
     {
-      path: "/fonts/agrandir/Agrandir-Regular.otf",
+      path: "./fonts/agrandir/Agrandir-Regular.otf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "/fonts/agrandir/Agrandir-Bold.otf",
+      path: "./fonts/agrandir/Agrandir-Bold.otf",
       weight: "700",
       style: "normal",
     },
@@ -22,22 +22,22 @@ export const graphik = localFont({
   display: "swap",
   src: [
     {
-      path: "/fonts/graphik/GraphikRegular.otf",
+      path: "./fonts/graphik/GraphikRegular.otf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "/fonts/graphik/GraphikMedium.otf",
+      path: "./fonts/graphik/GraphikMedium.otf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "/fonts/graphik/GraphikSemiBold.otf",
+      path: "./fonts/graphik/GraphikSemiBold.otf",
       weight: "600",
       style: "normal",
     },
     {
-      path: "/fonts/graphik/GraphikBold.otf",
+      path: "./fonts/graphik/GraphikBold.otf",
       weight: "700",
       style: "normal",
     },

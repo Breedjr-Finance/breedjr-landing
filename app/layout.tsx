a4dcdb5b8ee9acb48f1afbb7f5682f88e112f95a
Michael Akinsuroju <makinsuroju@gmail.com>
2025-06-08 12:25:58 +0100

    fix: fonts not found

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,50 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { agrandir, graphik } from './fonts';
-// import Navbar from "@/components/navigation/Navbar";
-// import Footer from "@/components/navigation/Footer";
+import localFont from "next/font/local";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
+const agrandir = localFont({
+  variable: "--font-agrandir",
+  display: "swap",
+  src: [
+    {
+      path: "/fonts/agrandir/Agrandir-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "/fonts/agrandir/Agrandir-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
 
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const graphik = localFont({
+  variable: "--font-graphik",
+  display: "swap",
+  src: [
+    {
+      path: "/fonts/graphik/GraphikRegular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "/fonts/graphik/GraphikMedium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "/fonts/graphik/GraphikSemiBold.otf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "/fonts/graphik/GraphikBold.otf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
